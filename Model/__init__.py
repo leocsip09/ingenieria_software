c6243c4dbde82030281d61674fb02fd9e5acc101
Kristopher <krospigliosig@unsa.edu.pe>
2024-07-31 02:21:37 -0500

    reformas hechas a la base de datos y su conexión y se agregó extensions.py para facilitar acceder a las extensiones de flask y flask-sqlalchemy

--- Model/__init__.py
+++ Model/__init__.py
@@ -1,14 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from Model.extensions import db
 
 app = Flask(__name__)
-
 app.config.from_object('config.Config')
 
-db = SQLAlchemy(app)
-
-with app.app_context():
-    db.create_all()
+db.init_app(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
