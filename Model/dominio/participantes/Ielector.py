56c572c2f619ffefff2d5f79560c66ad7d75b0bd
salimrod03 <131905784+salimrod03@users.noreply.github.com>
2024-07-31 16:20:43 -0500

    Ielector.py

--- Model/dominio/participantes/Ielector.py
+++ Model/dominio/participantes/Ielector.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
 
-class Ielector:
-    pass
+class IEditor(ABC):
+    #Elector
+    @abstractmethod
+    def registrar(self, nombre, apellido)->None:
+        pass 
+
+    @abstractmethod
+    def iniciar_sesion(self, correo, contraseña)->None:
+        pass 
+    
+    @abstractmethod
+    def votar(self)->None:
+        pass 
+    
+    def estado_voto(self)->None:
+        pass 
+
+    def editar_datos(self, nombre, apellido)->None:
+        pass 
+    
