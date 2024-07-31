ca11fc82de82fd274da2e19b3d4b5a71e97ed52b
Sebastian Himan <shiman@unsa.edu.pe>
2024-07-31 00:02:49 -0500

    Implementacion de la presentacion aplicando clean code

--- Model/presentacion/controladores/proceso_electoral_controller.py
+++ Model/presentacion/controladores/proceso_electoral_controller.py
@@ -1,7 +1,44 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-
+from flask import Blueprint, request, jsonify
 from servicios.Iproceso_electoral import Iproceso_electoral
+from servicios.proceso_electoral_servicio import ProcesoElectoralServicio
 
-class proceso_electoral_controller(Iproceso_electoral):
-    pass
+# Crear un blueprint para los procesos electorales
+proceso_electoral_bp = Blueprint('proceso_electoral', __name__)
+
+class ProcesoElectoralController:
+    def __init__(self):
+        self.servicio = ProcesoElectoralServicio()
+
+    @proceso_electoral_bp.route('/procesos-electorales', methods=['GET'])
+    def obtener_procesos(self):
+        """
+        Obtener todos los procesos electorales.
+        """
+        procesos = self.servicio.obtener_todos()
+        return jsonify(procesos)
+
+    @proceso_electoral_bp.route('/procesos-electorales', methods=['POST'])
+    def crear_proceso(self):
+        """
+        Crear un nuevo proceso electoral.
+        """
+        data = request.json
+        nuevo_proceso = self.servicio.crear(data)
+        return jsonify(nuevo_proceso), 201
+
+    @proceso_electoral_bp.route('/procesos-electorales/<int:id>', methods=['PUT'])
+    def actualizar_proceso(self, id):
+        """
+        Actualizar un proceso electoral existente.
+        """
+        data = request.json
+        proceso_actualizado = self.servicio.actualizar(id, data)
+        return jsonify(proceso_actualizado)
+
+    @proceso_electoral_bp.route('/procesos-electorales/<int:id>', methods=['DELETE'])
+    def eliminar_proceso(self, id):
+        """
+        Eliminar un proceso electoral existente.
+        """
+        self.servicio.eliminar(id)
+        return '', 204
